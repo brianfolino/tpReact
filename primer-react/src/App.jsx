2591fc1e40f5920cbbcfdf8d190fed27a97d1de0
brianfolino <brianfolino@hotmail.com>
2023-02-17 21:28:01 -0300

    s

--- primer-react/src/App.jsx
+++ primer-react/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
-
+import ItemDetailContainer from './comoponentes/ItemDetailContainer';
 import ItemListConteiner from './comoponentes/ItemListContainer';
 import NavBar from './comoponentes/NavBar';
-
+import Cart from './comoponentes/Cart';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
+
     <>
-    <NavBar/>
-    <ItemListConteiner/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+        <Route path='/' element={<ItemListConteiner />} />
+          <Route path='/categoria' element={<ItemListConteiner />} />
+          <Route path='detalle' element={<ItemDetailContainer />} />
+          <Route path='Cart' element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
